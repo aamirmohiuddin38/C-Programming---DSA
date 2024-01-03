22ac635b8f3ef172011e0a96b1d303b517240248
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-03 21:46:10 +0530

    HelloWorld program

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+    system("cls");
+    cout<<"Hello World! Welcome to C++"<<endl;
+    cout<<"---------------------------------\n\n";
+    return 0;
+}
