47b8e730b79f012282e20fe1cb7f923ab24ca859
aamirmohiuddin38 <eramirmohiuddin@gmail.com>
2024-01-16 12:37:28 +0530

    Int-float compatibility, floor ceil and round functions

--- FloatingPointNumbers.cpp
+++ FloatingPointNumbers.cpp
@@ -23,6 +23,31 @@ int main(void){
     cout<<"\t Type 1 = "<<fixed<<setprecision(1)<<fnum<<endl;
     cout<<"\t Type 2 = "<<fixed<<setprecision(2)<<fnum<<endl;
 
+    //Int-Float compatibility
+    int a, b;
+    float fa, fb;
+    cout<<"\n\t INT-FLOAT compatiblity..."<<endl;
+    cout<<"\t---------------------------------------"<<endl;
+
+    a = 2;
+    fa = a;
+    fb = fa * 2;
+    b = fb;
+
+    cout<<"\t a = "<<a<<endl;
+    cout<<"\t fa = "<<fa<<endl;
+    cout<<"\t fb = "<<fb<<endl;
+    cout<<"\t b = "<<b<<endl;
+
+    // FLOOR, CEIL and ROUND Functions
+    float fx = 3.14;
+    cout<<"\n\t FLOOR CEIL and ROUND function"<<endl;
+    cout<<"\t ---------------------------------------"<<endl;
+    cout<<"\tDefault fx = "<<fx<<endl;
+    cout<<"\tFloor fx = "<<floor(fx)<<endl;
+    cout<<"\tCeil fx = "<<ceil(fx)<<endl;
+    cout<<"\tRound fx = "<<round(fx)<<endl;
+
     cout<<endl;
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+    // cout<<"\t Type 1 = "<<fixed<<setprecision(1)<<fnum<<endl;
+    // cout<<"\t Type 2 = "<<fixed<<setprecision(2)<<fnum<<endl;
